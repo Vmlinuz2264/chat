d33d17748db36699762e20d6da13d7cba2ff6c78
kyleplo <31634240+kyleplo@users.noreply.github.com>
2017-12-28 08:39:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ for(var i = 0;i < text.length;i = i + 2){
 var nums = parseFloat(text.charAt(i) + text.charAt(i + 1));
 decoded += chars.charAt(nums - 1);
 }
+console.log("Decoded: " + decoded);
 return decoded;
 }
 function refresh(){
 document.getElementById("link").href = "https://scratch.mit.edu/projects/" + document.getElementById("server").value;
 fetch("https://cors-everywhere.herokuapp.com/clouddata.scratch.mit.edu/logs?projectid=" + document.getElementById("server").value + "&limit=15&offset=0").then(function (r){return r.json}).then(function (j){
-var dataHTML = "";
+console.log("Fetched: " + j);
+  var dataHTML = "";
 for(var x = 0;x < j.length;x++){
 var user = decode(j[x].value).split("|")[0];
 var text = decode(j[x].value).split("|")[1];
-dataHTML += "<p><a href='https://scratch.mit.edu/users/" + user + "'>" + user + "</a>: " + text + "</p>"
+dataHTML += "<p><a href='https://scratch.mit.edu/users/" + user + "'>" + user + "</a>: " + text + "</p>";
+  console.log(user + ": " + text);
 };
 document.getElementById("data").innerHTML = dataHTML;
-var d = new Date()
+  console.log(dataHTML);
+var d = new Date();
 document.getElementById("refresh").innerHTML = "Refreshed at: " + d.toTimeString();
+  console.log("Refreshed at: " + d.toTimeString());
 })
 }
 window.addEventListener("load",function (){refresh();setInterval(refresh,10000)})
