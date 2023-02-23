e41bbb940641f38f62b6349d8d587f9073affa2a
Vmlinuz2264 <121198893+Vmlinuz2264@users.noreply.github.com>
2023-02-22 19:45:59 -0600

    Update script.js
    Chat GPT

--- script.js
+++ script.js
@@ -12,27 +12,26 @@ function decode(text){
 function refresh(){
   document.getElementById("link").href = "https://scratch.mit.edu/projects/" + document.getElementById("server").value;
   fetch("https://api.allorigins.win/raw?url=https://clouddata.scratch.mit.edu/logs?projectid=" + document.getElementById("server").value + "&limit=40&offset=0")
-    .then(function (r){return r.json()})
-    .then(function (j){
-      console.log("Fetched: " + j);
-      var dataHTML = "";
-      for(var x = 0;x < j.length;x++){
-        var user = decode(j[x].value).split("|")[0];
-        var text = decode(j[x].value).split("|")[1];
-        var date = new Date(j[x].timestamp);
-        var datestring = timeago().format(date);
-        dataHTML += "<p><a href='https://scratch.mit.edu/users/" + user + "'>" + user + "</a>: " + text + " <i style='color:#ddd'>" + datestring + "</i></p>";
-        console.log(user + ": " + text);
-      };
-      document.getElementById("data").innerHTML = dataHTML;
-      console.log(dataHTML);
-      var d = new Date();
-      document.getElementById("refresh").innerHTML = "Refreshed at: " + d.toTimeString();
-      console.log("Refreshed at: " + d.toTimeString());
-    })
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
+  .then(function (r){
+    return r.json();
+  })
+  .then(function (j){
+    console.log("Fetched: " + j);
+    var dataHTML = "";
+    for(var x = 0;x < j.length;x++){
+      var user = decode(j[x].value).split("|")[0];
+      var text = decode(j[x].value).split("|")[1];
+      var date = new Date(j[x].timestamp);
+      var datestring = timeago().format(date);
+      dataHTML += "<p><a href='https://scratch.mit.edu/users/" + user + "'>" + user + "</a>: " + text + " <i style='color:#ddd'>" + datestring + "</i></p>";
+      console.log(user + ": " + text);
+    };
+    document.getElementById("data").innerHTML = dataHTML;
+    console.log(dataHTML);
+    var d = new Date();
+    document.getElementById("refresh").innerHTML = "Refreshed at: " + d.toTimeString();
+    console.log("Refreshed at: " + d.toTimeString());
+  })
 }
 
 window.addEventListener("load",function (){refresh();setInterval(refresh,10000)})
